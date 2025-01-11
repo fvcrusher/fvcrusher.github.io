166b858daea3f76976cc8c244a05f2b434400301
Vadim Karcev <cfg888powerfull@gmail.com>
2025-01-11 12:36:41 +0300

    Added iteratable mask for all subltls

--- LTL2Buchi/solver.js
+++ LTL2Buchi/solver.js
@@ -31,7 +31,7 @@ class Solver
         
     }
 
-    propagate_X(ltl, branch_X_count = 0)
+    #propagate_X(ltl, branch_X_count = 0)
     {
         switch (ltl.opc)
         {
@@ -46,12 +46,12 @@ class Solver
                 return atom;
 
             case Formula.Operator.X:
-                return this.propagate_X(ltl.lop, branch_X_count + 1);
+                return this.#propagate_X(ltl.lop, branch_X_count + 1);
 
             case Formula.Operator.NOT:
             case Formula.Operator.F:
             case Formula.Operator.G:
-                return Formula.unary(ltl.opc, this.propagate_X(ltl.lop, branch_X_count));
+                return Formula.unary(ltl.opc, this.#propagate_X(ltl.lop, branch_X_count));
 
             case Formula.Operator.AND:
             case Formula.Operator.OR:
@@ -59,13 +59,13 @@ class Solver
             case Formula.Operator.U:
             case Formula.Operator.W:
             case Formula.Operator.R:
-                return Formula.binary(ltl.opc, this.propagate_X(ltl.lop, branch_X_count), this.propagate_X(ltl.rop, branch_X_count));
+                return Formula.binary(ltl.opc, this.#propagate_X(ltl.lop, branch_X_count), this.#propagate_X(ltl.rop, branch_X_count));
         }
 
         return null;
     }
 
-    substitute_R(ltl)
+    #substitute_R(ltl)
     {
         switch (ltl.opc)
         {
@@ -81,8 +81,8 @@ class Solver
                     Formula.Operator.NOT,
                     Formula.binary(
                         Formula.Operator.U,
-                        Formula.unary(Formula.Operator.NOT, this.substitute_R(ltl.lop)),
-                        Formula.unary(Formula.Operator.NOT, this.substitute_R(ltl.rop))
+                        Formula.unary(Formula.Operator.NOT, this.#substitute_R(ltl.lop)),
+                        Formula.unary(Formula.Operator.NOT, this.#substitute_R(ltl.rop))
                     )
                 )
 
@@ -90,20 +90,20 @@ class Solver
             case Formula.Operator.NOT:
             case Formula.Operator.F:
             case Formula.Operator.G:
-                return Formula.unary(ltl.opc, this.substitute_R(ltl.lop));
+                return Formula.unary(ltl.opc, this.#substitute_R(ltl.lop));
 
             case Formula.Operator.AND:
             case Formula.Operator.OR:
             case Formula.Operator.IMPL:
             case Formula.Operator.U:
             case Formula.Operator.W:
-                return Formula.binary(ltl.opc, this.substitute_R(ltl.lop), this.substitute_R(ltl.rop));
+                return Formula.binary(ltl.opc, this.#substitute_R(ltl.lop), this.#substitute_R(ltl.rop));
         }
 
         return null;
     }
 
-    substitute_W(ltl)
+    #substitute_W(ltl)
     {
         switch (ltl.opc)
         {
@@ -118,10 +118,10 @@ class Solver
                 return Formula.binary(
                     Formula.Operator.OR,
                     Formula.binary(
-                        Formula.Operator.U, this.substitute_W(ltl.lop), this.substitute_W(ltl.rop)
+                        Formula.Operator.U, this.#substitute_W(ltl.lop), this.#substitute_W(ltl.rop)
                     ),
                     Formula.unary(
-                        Formula.Operator.G, this.substitute_W(ltl.lop)
+                        Formula.Operator.G, this.#substitute_W(ltl.lop)
                     )
                 )
 
@@ -129,20 +129,20 @@ class Solver
             case Formula.Operator.NOT:
             case Formula.Operator.F:
             case Formula.Operator.G:
-                return Formula.unary(ltl.opc, this.substitute_W(ltl.lop));
+                return Formula.unary(ltl.opc, this.#substitute_W(ltl.lop));
 
             case Formula.Operator.AND:
             case Formula.Operator.OR:
             case Formula.Operator.IMPL:
             case Formula.Operator.U:
             case Formula.Operator.R:
-                return Formula.binary(ltl.opc, this.substitute_W(ltl.lop), this.substitute_W(ltl.rop));
+                return Formula.binary(ltl.opc, this.#substitute_W(ltl.lop), this.#substitute_W(ltl.rop));
         }
 
         return null;
     }
 
-    substitute_G(ltl)
+    #substitute_G(ltl)
     {
         switch (ltl.opc)
         {
@@ -159,7 +159,7 @@ class Solver
                     Formula.unary(
                         Formula.Operator.F, 
                         Formula.unary(
-                            Formula.Operator.NOT, this.substitute_G(ltl.lop)
+                            Formula.Operator.NOT, this.#substitute_G(ltl.lop)
                         )
                     )
                 )
@@ -167,7 +167,7 @@ class Solver
             case Formula.Operator.X:
             case Formula.Operator.NOT:
             case Formula.Operator.F:
-                return Formula.unary(ltl.opc, this.substitute_G(ltl.lop));
+                return Formula.unary(ltl.opc, this.#substitute_G(ltl.lop));
 
             case Formula.Operator.AND:
             case Formula.Operator.OR:
@@ -175,13 +175,13 @@ class Solver
             case Formula.Operator.U:
             case Formula.Operator.W:
             case Formula.Operator.R:
-                return Formula.binary(ltl.opc, this.substitute_G(ltl.lop), this.substitute_G(ltl.rop));
+                return Formula.binary(ltl.opc, this.#substitute_G(ltl.lop), this.#substitute_G(ltl.rop));
         }
 
         return null;
     }
 
-    substitute_F(ltl)
+    #substitute_F(ltl)
     {
         switch (ltl.opc)
         {
@@ -193,12 +193,12 @@ class Solver
                 return new Formula(ltl.name);
 
             case Formula.Operator.F:
-                return Formula.binary(Formula.Operator.U, Formula.true(), this.substitute_F(ltl.lop));
+                return Formula.binary(Formula.Operator.U, Formula.true(), this.#substitute_F(ltl.lop));
 
             case Formula.Operator.X:
             case Formula.Operator.NOT:
             case Formula.Operator.G:
-                return Formula.unary(ltl.opc, this.substitute_F(ltl.lop));
+                return Formula.unary(ltl.opc, this.#substitute_F(ltl.lop));
 
             case Formula.Operator.AND:
             case Formula.Operator.OR:
@@ -206,21 +206,21 @@ class Solver
             case Formula.Operator.U:
             case Formula.Operator.W:
             case Formula.Operator.R:
-                return Formula.binary(ltl.opc, this.substitute_F(ltl.lop), this.substitute_F(ltl.rop));
+                return Formula.binary(ltl.opc, this.#substitute_F(ltl.lop), this.#substitute_F(ltl.rop));
         }
 
         return null;
     }
 
-    transform()
+    #transform()
     {
         this.transformation_steps = [];
         this.transformation_steps.push(this.ltl);
-        this.transformation_steps.push(this.propagate_X(this.ltl));
-        this.transformation_steps.push(this.substitute_R(this.ltl));
-        this.transformation_steps.push(this.substitute_W(this.ltl));
-        this.transformation_steps.push(this.substitute_G(this.ltl));
-        this.transformation_steps.push(this.substitute_F(this.ltl));
+        this.transformation_steps.push(this.#propagate_X(this.ltl));
+        this.transformation_steps.push(this.#substitute_R(this.ltl));
+        this.transformation_steps.push(this.#substitute_W(this.ltl));
+        this.transformation_steps.push(this.#substitute_G(this.ltl));
+        this.transformation_steps.push(this.#substitute_F(this.ltl));
 
         let tmp = [];
         
@@ -234,16 +234,16 @@ class Solver
         this.transformation_steps = tmp;
     }
 
-    find_variables(ltl=null)
+    #parse_subltls(ltl=null)
     {
         if (ltl == null)
             ltl = this.ltl;
 
         if (ltl.lop)
-            this.find_variables(ltl.lop);
+            this.#parse_subltls(ltl.lop);
 
         if (ltl.rop)
-            this.find_variables(ltl.rop);
+            this.#parse_subltls(ltl.rop);
 
         if (
             (ltl.opc == Formula.Operator.ATOM || ltl.opc == Formula.Operator.X) &&
@@ -258,6 +258,33 @@ class Solver
             this.#subltls.push(ltl);
     }
 
+    #clear_list(list)
+    {
+        while (list.pop());
+    }
+
+    #iterate_mask(mask)
+    {
+        if (mask.length != this.#variables.length)
+        {
+            this.#clear_list(mask);
+            for (let i = 0; i < this.#variables.length; i++)
+                mask.push(false);
+
+            return true;
+        }
+
+        let last_false = mask.lastIndexOf(false)
+        if (last_false == -1)
+            return false;
+
+        mask[last_false] = true;
+        for (let i = last_false + 1; i < mask.length; i++)
+            mask[i] = false;
+
+        return true;
+    }
+
     solve(ltl)
     {
         if (typeof ltl === "string")
@@ -268,17 +295,24 @@ class Solver
         else
             return;
 
-        this.transform();
-        this.find_variables();
+        this.#transform();
+        this.#parse_subltls();
+
+        let variables_mask = [];
+        while (this.#iterate_mask(variables_mask))
+        {
+            let all_mask = variables_mask.concat(Array(this.#subltls.length).fill(undefined));
+            console.log(all_mask);
+        }
     }
 }
 
 let solver = new Solver();
-solver.solve("Fa U Gb");
+solver.solve("Fa U Gb & c -> d");
 //solver.solve("x U (XFy & Fp) -> X(Gr | X(t & Xi & false) | s) & (o & p)");
 for (let i = 0; i < solver.transformation_steps.length; i++)
     console.log(solver.transformation_steps[i].string);
 
-console.log(`There is ${solver.all_subltls.length} variables`);
+console.log(`There is ${solver.all_subltls.length} subltls`);
 for (let i = 0; i < solver.all_subltls.length; i++)
     console.log(solver.all_subltls[i].string);
